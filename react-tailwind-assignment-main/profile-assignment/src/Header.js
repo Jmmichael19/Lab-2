409a6b356cd12bf21bd3a9d771cfe1e2d7417ee2
Jmmichael19 <167400971+Jmmichael19@users.noreply.github.com>
2025-10-01 18:41:11 +0800

    Updated Header.js

--- react-tailwind-assignment-main/profile-assignment/src/Header.js
+++ react-tailwind-assignment-main/profile-assignment/src/Header.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="flex items-center justify-center p-4 bg-blue-600 text-white shadow-md m-3">
+    <header className="flex items-center justify-center p-4 bg-blue-600 text-white shadow-md m-3 rounded">
       <nav>
-        <ul className="flex space-x-20">
-          <li className="cursor-pointer hover:text-white-600 transform transition-transform duration-200 hover:scale-105">
-            Home
+        <ul className="flex space-x-24">
+          <li className="cursor-pointer hover:text-white-600 transform transition-transform duration-200 hover:scale-105 font-bold">
+            HOME
           </li>
-          <li className="cursor-pointer hover:text-white-600 transform transition-transform duration-200 hover:scale-105">
-            Products
+          <li className="cursor-pointer hover:text-white-600 transform transition-transform duration-200 hover:scale-105 font-bold">
+            PRODUCTS
           </li>
-          <li className="cursor-pointer hover:text-white-600 transform transition-transform duration-200 hover:scale-105">
-            Contact
+          <li className="cursor-pointer hover:text-white-600 transform transition-transform duration-200 hover:scale-105 font-bold">
+            CONTACT
           </li>
         </ul>
       </nav>
